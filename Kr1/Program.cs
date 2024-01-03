c54a3ac5311094ecdc0b89ad9961b2efababf48b
Ekaterina Mitrofanova <emitrofanova8@gmail.com>
2024-01-03 20:43:02 +0400

    добавление комментариев к методам

--- Kr1/Program.cs
+++ Kr1/Program.cs
@@ -3,20 +3,20 @@ using System.Linq;
 
 class Program
 {
-    static void Main()
+    static void Main()// Задаем массив и выводим его
     {
         string[] stringArray = { "Hello", "World", "123", "456", "C#" };
         ProcessAndDisplayArray(stringArray);
     }
 
-        static T[] FilterByLength<T>(T[] originalArray, int maxLength)
+        static T[] FilterByLength<T>(T[] originalArray, int maxLength)// метод для фильтрации содержимого по длинне
     {
         var result = originalArray.Where(item => item != null && item.ToString().Length <= maxLength).ToArray();
         return result;
     }
 
   
-    static void DisplayArray<T>(T[] array)
+    static void DisplayArray<T>(T[] array)//Отображение в консоли
     {
         foreach (var item in array)
         {
@@ -25,7 +25,7 @@ class Program
     }
 
     
-    static void ProcessAndDisplayArray<T>(T[] originalArray)
+    static void ProcessAndDisplayArray<T>(T[] originalArray)// преобразование и отображение массива до и после фильтрации
     {
         while (true)
         {
